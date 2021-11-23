127828ef6f1623666ca747f16ded96919be6aa54
Shivangi Patodiya <shivu2610@gmail.com>
2021-11-23 08:36:07 -0700

    Add button functions for increase/decrease and remove items from cart

--- buy-now/src/components/Cart.js
+++ buy-now/src/components/Cart.js
@@ -1,45 +1,70 @@
 import ProductInCart from "./ProductInCart";
 import "./cart.scss";
-import { commerce } from '../commerce';
+import { commerce } from "../commerce";
 import { useState, useEffect } from "react";
-
-
+import { LoadingOutlined } from "@ant-design/icons";
+import { Skeleton, Card } from "antd";
+const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 
 function Cart() {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState({ subtotal: {} });
   const [lineItems, setLineItems] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const fetchCart = async() => {
+  const fetchCart = async () => {
     const cart = await commerce.cart.retrieve();
-    console.log(cart)
+    console.log("cart", cart);
     setLineItems(cart.line_items);
     setCart(cart);
-  }
+    setLoading(false);
+  };
+
   useEffect(() => {
+    setLoading(true);
     fetchCart();
   }, []);
 
   return (
     <div className="cart">
       <h1>My Cart</h1>
-
-      {lineItems.map((product) => {
-        return (
-          <div>
-            <ProductInCart
-              key={product.product_id}
-              cart_id={cart.id}
-              name={product.name}
-              product_id={product.product_id}
-              image={product.image.url}
-              cart_quantity={product.quantity}
-              price={product.price.formatted_with_symbol}
-            />
-          </div>
-        );
-      })}
-
-      <h3>Subtotal({cart.total_items} items):total {cart.subtotal.formatted_with_symbol}</h3>
+      {loading ? (
+        <div>
+          <Card>
+            <Skeleton avatar paragraph={{ rows: 4 }} active />
+          </Card>
+          <br />
+          <Card>
+            <Skeleton avatar paragraph={{ rows: 4 }} active />
+          </Card>
+          <br />
+          <Card>
+            <Skeleton avatar paragraph={{ rows: 4 }} active />
+          </Card>
+        </div>
+      ) : (
+        <div>
+          {lineItems.map((product) => {
+            return (
+              <ProductInCart
+                key={product.product_id}
+                cart_id={cart.id}
+                line_item_id={product.id}
+                name={product.name}
+                product_id={product.product_id}
+                image={product.image.url}
+                cart_quantity={product.quantity}
+                price={product.price.formatted_with_symbol}
+                fetchCart={fetchCart}
+              />
+            );
+          })}
+          <h3 style={{float: "right"}}>
+            Subtotal({cart.total_items} items):total{" "}
+            {cart.subtotal.formatted_with_symbol}
+          </h3>
+          <br/>
+        </div>
+      )}
     </div>
   );
 }
--- buy-now/src/components/ProductInCart.js
+++ buy-now/src/components/ProductInCart.js
@@ -1,17 +1,28 @@
 //import 'antd/dist/antd.css';
-import { Card,  Button } from "antd";
+import { Card, Button } from "antd";
 import { useState, useEffect } from "react";
-import { commerce } from '../commerce';
+import { commerce } from "../commerce";
 
 function ProductInCart(props) {
-  const [product, setProduct] = useState({is: {}});
-  
-  const fetchProduct = async() => {
-    const localProduct = await commerce.products.retrieve(props.product_id);
-    console.log(localProduct, "local")
-    setProduct(localProduct);
-  }
+  const [product, setProduct] = useState({ is: {} });
+  const [quantity, setQuantity] = useState(props.cart_quantity);
 
+  const fetchProduct = async () => {
+    const localProduct = await commerce.products.retrieve(props.product_id);
+    console.log(localProduct, "local");
+    setProduct(localProduct);
+  };
+  const changeQuantity = async (newQuantity) => {
+    const oldQuantity = quantity;
+    setQuantity(newQuantity);
+    try {
+      await commerce.cart.update(props.line_item_id, { quantity: newQuantity });
+      props.fetchCart();
+    } catch (e) {
+      console.log(e);
+      setQuantity(oldQuantity);
+    }
+  };
   useEffect(() => {
     fetchProduct();
   }, []);
@@ -23,19 +34,19 @@ function ProductInCart(props) {
           <img height="200px" src={props.image} alt={props.name}></img>
           <span className="cart-product-detail">
             <h3>{props.name}</h3>
-            <p dangerouslySetInnerHTML={{__html: product.description}}/>
+            {/* <p dangerouslySetInnerHTML={{ __html: product.description }} /> */}
             <p>{props.price}</p>
             <p>{product.is.sold_out ? "Out of Stock" : "In Stock"}</p>
             <span className="cart-edit">
               <div>
-                <Button>-</Button>
+                <Button onClick={() => changeQuantity(quantity - 1)}>-</Button>
                 &nbsp;
-                <div>{props.cart_quantity}</div>
+                <div>{quantity}</div>
                 &nbsp;
-                <Button>+</Button>
+                <Button onClick={() => changeQuantity(quantity + 1)}>+</Button>
               </div>
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <Button type="primary" danger>
+              <Button type="primary" danger onClick={()=>changeQuantity(0)}>
                 <strong>Remove</strong>
               </Button>
             </span>
