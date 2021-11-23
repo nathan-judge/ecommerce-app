38e60df6b397b0dac5061d9b341df8bdec46c36b
Shivangi Patodiya <shivu2610@gmail.com>
2021-11-22 17:39:28 -0700

    add cart UI

--- buy-now/src/components/Cart.js
+++ buy-now/src/components/Cart.js
@@ -1,57 +1,45 @@
 import ProductInCart from "./ProductInCart";
 import "./cart.scss";
-const carts = [
-  {
-    product_id: 1,
-    name: "Shoes",
-    description: "Red shoes for hiking",
-    image: 'https://m.media-amazon.com/images/I/61OckGLlZUL._AC_UL320_.jpg',
-    product_quantity: 50,
-    number_of_items: 12,
-    price: 120
-  },
-  {
-    product_id: 2,
-    name: "Jacket",
-    description: "Blue jacket for winter",
-    image: 'https://m.media-amazon.com/images/I/61OckGLlZUL._AC_UL320_.jpg',
-    product_quantity: 25,
-    number_of_items: 1,
-    price: 1220
-  },
-  {
-    product_id: 3,
-    name: "Hats",
-    description: "Cool hat for summer",
-    image: 'https://m.media-amazon.com/images/I/61OckGLlZUL._AC_UL320_.jpg',
-    product_quantity: 150,
-    number_of_items: 3,
-    price: 20
-  }
-];
+import { commerce } from '../commerce';
+import { useState, useEffect } from "react";
+
+
 
 function Cart() {
+  const [cart, setCart] = useState([]);
+  const [lineItems, setLineItems] = useState([]);
+
+  const fetchCart = async() => {
+    const cart = await commerce.cart.retrieve();
+    console.log(cart)
+    setLineItems(cart.line_items);
+    setCart(cart);
+  }
+  useEffect(() => {
+    fetchCart();
+  }, []);
+
   return (
     <div className="cart">
       <h1>My Cart</h1>
 
-      {carts.map((product) => {
+      {lineItems.map((product) => {
         return (
           <div>
             <ProductInCart
+              key={product.product_id}
+              cart_id={cart.id}
               name={product.name}
-              product-id={product.product_id}
-              image={product.image}
-              description={product.description}
-              cart_quantity={product.number_of_items}
-              product_quantity={product.product_quantity}
-              price={product.price}
+              product_id={product.product_id}
+              image={product.image.url}
+              cart_quantity={product.quantity}
+              price={product.price.formatted_with_symbol}
             />
           </div>
         );
       })}
 
-      <h3>Subtotal(2 items):total $$</h3>
+      <h3>Subtotal({cart.total_items} items):total {cart.subtotal.formatted_with_symbol}</h3>
     </div>
   );
 }
