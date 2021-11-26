1bdfc45e54c983a823172edda090f8fac49acc9d
Paul <paul.wei607@gmail.com>
2021-11-26 14:25:05 -0800

    makes messages work

--- buy-now/src/App.js
+++ buy-now/src/App.js
@@ -8,12 +8,17 @@ import Cart from "./components/Cart";
 import Navbar from "./components/Navbar";
 import AdminDashboard from "./components/admin/AdminDashboard";
 import helpers from "./helpers/cartSubtotal"
+import { Widget, addResponseMessage } from "react-chat-widget"
+import "react-chat-widget/lib/styles.css";
+import {io} from 'socket.io-client'
+const socket = io('http://localhost:8000');
 
 const App = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
   const [itemsCount, setItemsCount] = useState(0);
   const [subtotal, setSubtotal] = useState(0);
+
   
   const fetchCart = async () => {
     try {
@@ -31,6 +36,21 @@ const App = () => {
       console.log("Error fetching new cart", e);
     }
   };
+
+  useEffect(() => {
+    addResponseMessage('Welcome to BuyNow! If you have any questions regarding this store, feel free to type it in the chat, we will get back to you shortly!')
+    socket.on('receive-message', (message) => {
+      console.log("MESSAGE IS: ", message)
+      addResponseMessage(message);
+    });
+  }, []);
+  const handleNewUserMessage = (newMessage) => {
+    console.log(`New message incoming! ${newMessage}`)
+    //to send message use 'emit' emit will emit messages including themselves (broadcast doesn't include themselves)
+    socket.emit('send-message', newMessage);
+    //to listen for messages use 'on'
+
+  }
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -52,6 +72,7 @@ const App = () => {
         <Route path="/cart" element={<Cart cart={cart} itemsCount={itemsCount} subtotal={subtotal} fetchCart={fetchCart} setCart={setCart} />} />
         <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
+      <Widget handleNewUserMessage={handleNewUserMessage}/>
     </BrowserRouter>
   );
 };
--- buy-now/src/components/ProductList.js
+++ buy-now/src/components/ProductList.js
@@ -1,26 +1,9 @@
 import React from "react";
 import ProductListItem from "./ProductListItem";
-import { Widget, addResponseMessage } from "react-chat-widget"
-import "react-chat-widget/lib/styles.css";
-import {useEffect} from "react";
-import {io} from 'socket.io-client'
-const socket = io('http://localhost:8000');
+
 
 export default function ProductList(props) {
-  useEffect(() => {
-    addResponseMessage('Welcome to BuyNow! If you have any questions regarding this store, feel free to type it in the chat, we will get back to you shortly!')
-  }, []);
-
-  const handleNewUserMessage = (newMessage) => {
-    console.log(`New message incoming! ${newMessage}`)
-    //to send message use 'emit' emit will emit messages including themselves (broadcast doesn't include themselves)
-    socket.emit('send-message', newMessage);
-    //to listen for messages use 'on'
-    socket.on('receive-message', (message) => {
-      addResponseMessage(message);
-    })
-   
-  }
+  
   const listProduct = props.products.map((product, key) => {
     return (
       <ProductListItem
@@ -41,7 +24,7 @@ export default function ProductList(props) {
     <main>
       <h1>Products</h1>
       {listProduct}
-      <Widget handleNewUserMessage={handleNewUserMessage}/>
+     
     </main>
   );
 }
